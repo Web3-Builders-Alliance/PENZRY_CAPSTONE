7db7903b5dd629dee0ac5f91bb25973a3af44958
Codeindeed <favouralex084@gmail.com>
2024-03-03 12:45:30 +0100

    removed helius api key

--- src/Services/axios/helius.ts
+++ src/Services/axios/helius.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  "https://devnet.helius-rpc.com/?api-key=e4565d31-8f06-40e9-b783-2e42a4e229f1";
+  "https://devnet.helius-rpc.com/?api-key=";
 
 export default axios.create({
   baseURL: BASE_URL,
